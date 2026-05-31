cc1faeced3ec2994e12bd61b38eb9d40cb2c3537
harshtiwariexe <harsh.official1802@gmail.com>
2026-05-31 23:28:26 +0530

    Established connection pipeline

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
+#include <string.h>
 
 int main()
 {
@@ -14,11 +15,23 @@ int main()
     getaddrinfo(NULL, "8080", &hints, &res);
 
     int bind_res = bind(fd, res->ai_addr, res->ai_addrlen);
+    printf("Bind result %d\n", bind_res);
 
-    while (1)
+    int listen_res = listen(fd, 20);
+    if (listen_res < 0)
     {
-        printf("Waitng for request.....\n");
-        sleep(1);
+        perror("Listen Error");
     }
+
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size = sizeof their_addr;
+    int new_fd = accept(fd, (struct sockaddr *)&their_addr, &addr_size);
+
+    int max_len = 1000;
+    char http_req[max_len];
+    memset(http_req, 0, max_len);
+    int num_bytes = recv(new_fd, http_req, max_len, 0);
+
+    puts(http_req);
     return 0;
 }
